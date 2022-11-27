0538d78e19c4be39fd5c13dd49e8554ad3a9a55d
koushik-00 <phaneendrakoushik@gmail.com>
2022-11-27 16:56:57 +0530

    L7

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8432;
 app.listen(port, () => {
   console.log(`server listening at port - ${port}`);
 });
